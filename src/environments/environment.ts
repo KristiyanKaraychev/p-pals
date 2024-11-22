feb7990b5d307a7bfd0942e413d830c14e34cb49
Kristiyan Karaychev <k.karaychev@gmail.com>
2024-11-22 19:26:18 +0200

    Configured Firebase SDK

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyD4Y4HEigV4xANCkDWO1Uyq5L0yDouq71Y',
+        authDomain: 'p-pals.firebaseapp.com',
+        databaseURL:
+            'https://p-pals-default-rtdb.europe-west1.firebasedatabase.app',
+        projectId: 'p-pals',
+        storageBucket: 'p-pals.firebasestorage.app',
+        messagingSenderId: '272599448436',
+        appId: '1:272599448436:web:c6fb01d4b4f0a11d140a21',
+    },
+};
